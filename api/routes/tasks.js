5d08f9c429f4c4960ccd5953a691cd971fb04876
Alexandre Leroux <alex@sherpa.one>
2023-01-24 11:51:23 +0100

    post task

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -2,11 +2,21 @@ const express = require('express');
 const router = express.Router();
 const db = require("../db_connection");
 
-
 router.get('/', async (req, res, next) => {
     try {
         const tasks = await db('tasks');
         res.json({ data: tasks });
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        const { content, user_id } = req.body;
+        await db('tasks').insert({ content, user_id });
+        res.sendStatus(201);
     } catch (error) {
         console.error(error);
         next(error);
